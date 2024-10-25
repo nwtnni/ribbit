d18f951574b73e8e72766133f43637b7eff75475
Newton Ni <nwtnni@gmail.com>
2024-10-25 10:47:25 -0500

    Test generated code for tuples

--- ribbit/tests/tuple.rs
+++ ribbit/tests/tuple.rs
@@ -1,7 +1,25 @@
-#[ribbit::pack(size = 32)]
-#[derive(Copy, Clone)]
-pub struct OneNative(u32);
+use core::num::NonZeroU64;
 
-#[ribbit::pack(size = 64, nonzero)]
-#[derive(Copy, Clone)]
-pub struct OneNonZero(NonZeroU64);
+#[test]
+fn one_native() {
+    #[ribbit::pack(size = 32)]
+    #[derive(Copy, Clone)]
+    pub struct OneNative(u32);
+
+    let native = OneNative::new(5);
+    assert_eq!(native._0(), 5);
+    let native = native.with_0(12);
+    assert_eq!(native._0(), 12);
+}
+
+#[test]
+fn one_non_zero() {
+    #[ribbit::pack(size = 64, nonzero)]
+    #[derive(Copy, Clone)]
+    pub struct OneNonZero(NonZeroU64);
+
+    let non_zero = OneNonZero::new(NonZeroU64::MIN);
+    assert_eq!(non_zero._0().get(), 1);
+    let non_zero = non_zero.with_0(NonZeroU64::new(135).unwrap());
+    assert_eq!(non_zero._0().get(), 135);
+}
