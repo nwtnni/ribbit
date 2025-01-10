894e48ba4db990ad694d54833a53b2c4894fffec
Newton Ni <nwtnni@gmail.com>
2025-01-10 16:08:01 -0600

    Catch generics in NonZero implementation

--- ribbit-derive/src/gen/repr.rs
+++ ribbit-derive/src/gen/repr.rs
@@ -14,13 +14,13 @@ pub(crate) fn repr(
         ..
     }: &ir::Ir,
 ) -> TokenStream {
-    let nonzero = match *repr.nonzero {
-        true => quote!(unsafe impl ::ribbit::NonZero for #ident {}),
-        false => quote!(),
-    };
-
     let size = repr.size();
     let (r#impl, ty, r#where) = generics.split_for_impl();
+
+    let nonzero = match *repr.nonzero {
+        true => quote!(unsafe impl #r#impl ::ribbit::NonZero for #ident #ty #r#where {}),
+        false => quote!(),
+    };
 
     // https://github.com/MrGVSV/to_phantom/blob/main/src/lib.rs
     let lifetimes = generics.lifetimes().map(|lifetime| quote!(&#lifetime ()));
--- ribbit/tests/zst.rs
+++ ribbit/tests/zst.rs
@@ -1,4 +1,5 @@
 use core::marker::PhantomData;
+use core::num::NonZeroU64;
 
 #[test]
 fn custom_zst() {
@@ -37,4 +38,26 @@ fn phantom() {
     let h = Phantom::<usize>::new(0xdead_beef);
     assert_eq!(h.value, 0xdead_beef);
     assert_eq!(h.a(), 0xdead_beef);
+}
+
+#[test]
+fn phantom_nonzero() {
+    #[ribbit::pack(size = 64, nonzero)]
+    struct Phantom<A> {
+        a: NonZeroU64,
+        #[ribbit(size = 0)]
+        foo: PhantomData<A>,
+    }
+
+    impl<A> Copy for Phantom<A> {}
+
+    impl<A> Clone for Phantom<A> {
+        fn clone(&self) -> Self {
+            *self
+        }
+    }
+
+    let h = Phantom::<usize>::new(NonZeroU64::new(0xdead_beef).unwrap());
+    assert_eq!(h.value.get(), 0xdead_beef);
+    assert_eq!(h.a().get(), 0xdead_beef);
 }
