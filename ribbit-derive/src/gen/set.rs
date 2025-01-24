24b7e803a15d0fb943bfffe4c6d963a5f667a7f7
Newton Ni <nwtnni@gmail.com>
2025-01-24 14:13:08 -0600

    Simplify with for newtype

--- ribbit-derive/src/gen/set.rs
+++ ribbit-derive/src/gen/set.rs
@@ -3,7 +3,7 @@ use core::ops::Deref;
 
 use proc_macro2::TokenStream;
 use quote::quote;
-use quote::ToTokens as _;
+use quote::ToTokens;
 
 use crate::ir;
 use crate::lift::Lift as _;
@@ -14,7 +14,9 @@ pub(crate) fn set<'ir>(
     ir::Ir { tight, data, .. }: &'ir ir::Ir,
 ) -> impl Iterator<Item = TokenStream> + 'ir {
     match data {
-        ir::Data::Struct(r#struct) => Or::L(
+        ir::Data::Struct(r#struct) => Or::L({
+            let newtype = r#struct.is_newtype();
+
             r#struct
                 .fields()
                 .map(
@@ -26,24 +28,32 @@ pub(crate) fn set<'ir>(
                          ..
                      }| { (vis, ident, ty.deref().clone(), *offset) },
                 )
-                .map(|(vis, ident, ty_field, offset)| {
+                .map(move |(vis, ident, ty_field, offset)| {
                     let ty_struct = ty::Tree::from(**tight);
                     let ty_struct_loose = tight.loosen();
 
-                    let escaped = ident.escaped();
+                    let escaped = ident.escaped().to_token_stream();
 
-                    #[allow(clippy::precedence)]
-                    let value_field = Box::new(
-                        escaped.to_token_stream().lift() % ty_field.clone() % ty_struct_loose
-                            << offset,
-                    );
+                    let value_struct = match newtype {
+                        true if ty_field.is_leaf() => escaped.clone(),
+                        true => (escaped.clone().lift() % ty_field.clone() % ty_struct)
+                            .to_token_stream(),
+                        #[allow(clippy::precedence)]
+                        false => {
+                            let value_field = Box::new(
+                                escaped.to_token_stream().lift()
+                                    % ty_field.clone()
+                                    % ty_struct_loose
+                                    << offset,
+                            );
 
-                    let clear = !(ty_field.mask() << offset) & ty_struct.mask();
+                            let clear = !(ty_field.mask() << offset) & ty_struct.mask();
 
-                    #[allow(clippy::precedence)]
-                    let value_struct = (quote!(self.value).lift() % ty_struct.clone() & clear
-                        | value_field)
-                        % ty_struct;
+                            (quote!(self.value).lift() % ty_struct.clone() & clear | value_field)
+                                % ty_struct
+                        }
+                        .to_token_stream(),
+                    };
 
                     let with = ident.unescaped("with");
                     quote! {
@@ -56,8 +66,8 @@ pub(crate) fn set<'ir>(
                             }
                         }
                     }
-                }),
-        ),
+                })
+        }),
         ir::Data::Enum(_) => Or::R(iter::empty()),
     }
 }
